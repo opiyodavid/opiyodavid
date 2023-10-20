3537c480e2f3c01ebb4ba2b422c9a7b39e801b9f
opiyodavid <146334570+opiyodavid@users.noreply.github.com>
2023-10-20 15:14:39 +0300

    Add files via upload

---
+++ Dbconnect.php
@@ -0,0 +1,7 @@
+<?php
+$servername="localhost";
+$user="root";
+$password="";
+$databasename="MSU library";
+$conn=new mysqli($servername,$user,$password,$databasename);
+?>
---
+++ insert.php
@@ -0,0 +1,56 @@
+body {
+    font-family: Arial, sans-serif;
+    background-color: #f4f4f4;
+    margin: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+}
+
+form {
+    background-color: #fff;
+    padding: 20px;
+    border-radius: 8px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    max-width: 400px;
+    width: 100%;
+}
+
+h1 {
+    text-align: center;
+    color: #333;
+}
+
+p {
+    margin-bottom: 10px;
+}
+
+input[type="text"] {
+    width: 100%;
+    padding: 10px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    font-size: 16px;
+}
+
+button {
+    display: inline-block;
+    padding: 10px 20px;
+    font-size: 16px;
+    font-weight: bold;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+    margin-right: 10px;
+}
+
+button[type="submit"] {
+    background-color: #007bff;
+    color: #fff;
+}
+
+button[type="reset"] {
+    background-color: #ccc;
+    color: #333;
+}
